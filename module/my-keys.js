424804c6b727e03cdf5076078b6b9fd3bcb3ca2d
mytoh <mytoh@gmail.com>
2013-12-22 11:28:50 +0900

    module/my-keys.js: add minibuffer keymap

--- module/my-keys.js
+++ module/my-keys.js
@@ -8,4 +8,7 @@ define_key_alias("C-m", "return");
 define_key_alias("C-i", "tab");
 define_key_alias("C-[", "escape");
 
+// mini buffer
+define_key(minibuffer_keymap, "C-h", "cmd_deleteCharBackward");
+
 provide("my-keys");
