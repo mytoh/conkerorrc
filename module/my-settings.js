089e9db76b7f243b66a301801cce483f8f1cfae4
mytoh <mytoh@gmail.com>
2014-05-17 05:39:50 +0900

    update

--- module/my-settings.js
+++ module/my-settings.js
@@ -16,6 +16,23 @@ cwd = get_home_directory();
 cwd.append("huone");
 cwd.append("lataukset");
 
+// remember last download directory
+// {
+//    let _save_path = my_download_path;
+
+//    function update_save_path (info) {
+//        _save_path = info.target_file.parent.path;
+//    }
+
+//    add_hook("download_added_hook", update_save_path);
+
+//    suggest_save_path_from_file_name = function (filename, buffer) {
+//        let file = make_file(_save_path);
+//        file.append(filename);
+//        return file.path;
+//    }
+// }
+
 
 // load urls from the command line in new buffers instead
 // of new windows.
