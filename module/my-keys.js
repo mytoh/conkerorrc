a163a5365003cbdaa2b100b61cd9b95efab07d75
mytoh <mytoh@gmail.com>
2015-09-02 13:06:36 +0900

    add keys

--- module/my-keys.js
+++ module/my-keys.js
@@ -10,6 +10,11 @@ define_key_alias("C-[", "escape");
 define_key(default_global_keymap,"H", "home");
 define_key(default_global_keymap,"Q", "kill-current-buffer");
 define_key(default_global_keymap,"q", "escape");
+define_key(default_global_keymap, "k","cmd_scrollLineUp");
+define_key(default_global_keymap, "j","cmd_scrollLineDown");
+define_key(default_global_keymap, "C-u","cmd_scrollPageUp");
+define_key(default_global_keymap, "C-d","cmd_scrollPageDown");
+define_key(default_global_keymap, "M-u","universal-argument");
 
 //// http://emacs-fu.blogspot.jp/2010/12/conkeror-web-browsing-emacs-way.html
 // copy url with C-c u
