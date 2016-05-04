ab783899cb12225f9096769f59524444d48de1ad
mytoh <mytoh@gmail.com>
2016-05-04 14:45:44 +0900

    * module/my-modeline.js: adjust widgets

--- module/my-modeline.js
+++ module/my-modeline.js
@@ -3,18 +3,25 @@ dumpln("my-modeline");
 
 require("mode-line-buttons");
 
-/// modeline
-// add buttons
-mode_line_add_buttons(standard_mode_line_buttons, true);
-// download
-add_hook("mode_line_hook", mode_line_adder(downloads_status_widget));
-// favicon
-add_hook("mode_line_hook", mode_line_adder(buffer_icon_widget), true);
+
 // To enable favicons in the read_buffer completions list:
 read_buffer_show_icons = true;
+
+/// modeline
+
+// loading count
+add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
+
+// add buttons
+mode_line_add_buttons(standard_mode_line_buttons, true);
+
+// download
+add_hook("mode_line_hook", mode_line_adder(downloads_status_widget));
+
+// favicon
+add_hook("mode_line_hook", mode_line_adder(buffer_icon_widget), true);
+
 // To enable window buffer count in the mode-line:
 add_hook("mode_line_hook", mode_line_adder(buffer_count_widget), true);
-
-add_hook("mode_line_hook", mode_line_adder(loading_count_widget), true);
 
 provide("my-modeline");
